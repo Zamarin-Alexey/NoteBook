a649f091b0d5f2b297f25b29fc5b9c6c1dec7d97
Varvara Yakovleva <yakovleva.varvaraa@gmail.com>
2021-12-28 01:37:50 +0300

    Add my_notes page

--- app/controllers/notebook_controller.rb
+++ app/controllers/notebook_controller.rb
@@ -1,5 +1,8 @@
 class NotebookController < ApplicationController
   skip_before_action :require_login, only: %i[main_page cook_page]
   def my_notes
+    @notes = Note.all.map do |n|
+      { id: n.id, header: n.header, content: n.content, user_id: n.user_id }
+    end
   end
 end
