67001bba15d6f10a97bdf8e86567ea58bfa918e0
Varvara Yakovleva <yakovleva.varvaraa@gmail.com>
2021-12-28 02:28:59 +0300

    Some changes my_notes page

--- app/controllers/notebook_controller.rb
+++ app/controllers/notebook_controller.rb
@@ -1,7 +1,7 @@
 class NotebookController < ApplicationController
   skip_before_action :require_login, only: %i[main_page cook_page]
   def my_notes
-    @notes = Note.all.map do |n|
+    @notes = Note.where(user_id: current_user.id).all.map do |n|
       { id: n.id, header: n.header, content: n.content, user_id: n.user_id }
     end
   end
